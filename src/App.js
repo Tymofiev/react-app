ace2262988388f59c314ece266c9da142226335b
Tymofiev <ilia.tumofiev@gmail.com>
2020-01-10 18:14:31 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LoginForm extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { login: '', pass: '', errors: [] }
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	showError(id, msg) {
+		this.setState((prevState) => ({
+			errors: [...prevState.errors, { id, msg }],
+		}))
+	}
+
+	clearError(id) {
+		this.setState((prevState) => {
+			let newArray = []
+			for (let err of prevState.errors) {
+				if (id !== err.id) {
+					newArray.push(err)
+				}
+			}
+			return newArray
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+
+		this.setState({ errors: [] })
+
+		let validLogin = 'login'
+		let validPass = 'pass'
+
+		if (this.state.login === '' || this.state.pass === '') {
+			this.showError('empty', 'Login and password cannot be blank')
+			return
+		} else if (
+			validLogin !== this.state.login &&
+			validPass !== this.state.pass
+		) {
+			this.showError('incorrect', 'Invalid login or password')
+			return
+		}
+
+		alert('Succesfully logged in')
+
+		return <Redirect to='/cabinet' />
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<h1>Login form</h1>
+				</header>
+				<section className='App-content'>
+					<div className='container'>
+						<div className='row d-flex justify-content-center'>
+							<div className='col-md-6'>
+								<Form onSubmit={this.handleSubmit}>
+									<Form.Group controlId='formBasicEmail'>
+										<Form.Label>Login</Form.Label>
+										<Form.Control
+											name='login'
+											type='text'
+											value={this.state.login}
+											onChange={this.handleChange}
+											placeholder='Enter login'
+										/>
+									</Form.Group>
+
+									<Form.Group controlId='formBasicPassword'>
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											name='pass'
+											type='password'
+											value={this.state.pass}
+											onChange={this.handleChange}
+											placeholder='Password'
+										/>
+									</Form.Group>
+									{this.state.errors
+										? this.state.errors.map((error) => (
+												<small className='errors' key={error.id}>
+													{error.msg}
+												</small>
+										  ))
+										: ''}
+									<Button
+										className='btn btn-block submit_button'
+										variant='primary'
+										type='submit'
+									>
+										Log in
+									</Button>
+								</Form>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		)
+	}
 }
 
-export default App;
+class Cabinet extends Component {
+	render() {
+		return <div>Succesfully logged in!</div>
+	}
+}
+
+class Routes extends Component {
+	render() {
+		return (
+			<Switch>
+				<Route exact={true} path='/' component={LoginForm} />
+				<Route path='/cabinet' component={Cabinet} />
+			</Switch>
+		)
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<Routes />
+			</Router>
+		)
+	}
+}
+
+export default App
