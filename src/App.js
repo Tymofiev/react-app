e570ec6340a93d385067cfa449a38210acaae87c
Tymofiev <ilia.tumofiev@gmail.com>
2020-01-17 13:49:18 +0200

    register form implemented

--- src/App.js
+++ src/App.js
@@ -1,150 +1,18 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'react-bootstrap'
-import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import './App.css'
-import axios from 'axios'
-
-class LoginForm extends Component {
-	constructor(props) {
-		super(props)
-
-		this.state = { login: '', pass: '', errors: [] }
-
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	}
-
-	showError(id, msg) {
-		this.setState((prevState) => ({
-			errors: [...prevState.errors, { id, msg }],
-		}))
-	}
-
-	clearError(id) {
-		this.setState((prevState) => {
-			let newArray = []
-			for (let err of prevState.errors) {
-				if (id !== err.id) {
-					newArray.push(err)
-				}
-			}
-			return newArray
-		})
-	}
-
-	handleSubmit(event) {
-		event.preventDefault()
-
-		let login = this.state.login
-		let password = this.state.pass
-
-		let loginSuccess = false
-
-		axios({
-			method: 'post',
-			url: 'http://localhost:3001/users/login',
-			data: {
-				login: login,
-				lastName: password,
-			},
-		}).then((result) => {
-			loginSuccess = result.data
-		})
-
-		this.setState({ errors: [] })
-
-		if (login === '' || password === '') {
-			this.showError('empty', 'Login and password cannot be blank')
-			return
-		} else if (!loginSuccess) {
-			this.showError('incorrect', 'Invalid login or password')
-			return
-		}
-
-		alert('Succesfully logged in ', login)
-
-		return <Redirect to='/cabinet' />
-	}
-
-	handleChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value,
-		})
-	}
-
-	render() {
-		return (
-			<div className='App'>
-				<header className='App-header'>
-					<h1>Login form</h1>
-				</header>
-				<section className='App-content'>
-					<div className='container'>
-						<div className='row d-flex justify-content-center'>
-							<div className='col-md-6'>
-								<Form onSubmit={this.handleSubmit}>
-									<Form.Group controlId='formBasicEmail'>
-										<Form.Label>Login</Form.Label>
-										<Form.Control
-											name='login'
-											type='text'
-											value={this.state.login}
-											onChange={this.handleChange}
-											placeholder='Enter login'
-										/>
-									</Form.Group>
-
-									<Form.Group controlId='formBasicPassword'>
-										<Form.Label>Password</Form.Label>
-										<Form.Control
-											name='pass'
-											type='password'
-											value={this.state.pass}
-											onChange={this.handleChange}
-											placeholder='Password'
-										/>
-									</Form.Group>
-									{this.state.errors
-										? this.state.errors.map((error) => (
-												<small className='errors' key={error.id}>
-													{error.msg}
-												</small>
-										  ))
-										: ''}
-									<Button
-										className='btn btn-block submit_button'
-										variant='primary'
-										type='submit'
-									>
-										Log in
-									</Button>
-								</Form>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		)
-	}
-}
-
-class Cabinet extends Component {
-	render() {
-		return <div>Succesfully logged in!</div>
-	}
-}
+import RegisterForm from './components/registerform'
+import LoginForm from './components/loginform'
+import Home from './components/home'
 
 class Routes extends Component {
 	render() {
 		return (
 			<Switch>
-				<Route exact={true} path='/' component={LoginForm} />
-				<Route path='/cabinet' component={Cabinet} />
+				<Route exact={true} path='/' component={Home} />
+				<Route path='/login' component={LoginForm} />
+				<Route path='/register' component={RegisterForm} />
 			</Switch>
 		)
 	}
---
+++ src/components/loginform.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import axios from 'axios'
+
+export default class LoginForm extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { login: '', pass: '', errors: [] }
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	showError(id, msg) {
+		this.setState((prevState) => ({
+			errors: [...prevState.errors, { id, msg }],
+		}))
+	}
+
+	clearError(id) {
+		this.setState((prevState) => {
+			let newArray = []
+			for (let err of prevState.errors) {
+				if (id !== err.id) {
+					newArray.push(err)
+				}
+			}
+			return { errors: newArray }
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+
+		let login = this.state.login
+		let password = this.state.pass
+
+		let loginSuccess = false
+
+		if (login === '' || password === '') {
+			this.clearError('empty')
+			this.showError('empty', 'Login and password cannot be blank')
+			return
+		}
+
+		axios({
+			method: 'post',
+			url: 'http://localhost:3001/users/login',
+			data: {
+				login: login,
+				password: password,
+			},
+		}).then((result) => {
+			loginSuccess = result.data
+
+			if (!loginSuccess) {
+				this.showError('incorrect', 'Invalid login or password')
+				return
+			}
+
+			return this.props.history.push('/')
+		})
+
+		this.setState({ errors: [] })
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<h1>Login form</h1>
+				</header>
+				<section className='App-content'>
+					<div className='container'>
+						<div className='row d-flex justify-content-center'>
+							<div className='col-md-6'>
+								<Form onSubmit={this.handleSubmit}>
+									<Form.Group controlId='formBasicEmail'>
+										<Form.Label>Login</Form.Label>
+										<Form.Control
+											name='login'
+											type='text'
+											value={this.state.login}
+											onChange={this.handleChange}
+											placeholder='Enter login'
+										/>
+									</Form.Group>
+
+									<Form.Group controlId='formBasicPassword'>
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											name='pass'
+											type='password'
+											value={this.state.pass}
+											onChange={this.handleChange}
+											placeholder='Password'
+										/>
+									</Form.Group>
+									{this.state.errors
+										? this.state.errors.map((error) => (
+												<small className='errors' key={error.id}>
+													{error.msg}
+												</small>
+										  ))
+										: ''}
+									<Button
+										className='btn btn-block submit_button'
+										variant='primary'
+										type='submit'
+									>
+										Log in
+									</Button>
+								</Form>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		)
+	}
+}
